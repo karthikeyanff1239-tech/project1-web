2700bd1d67ffd751b4f603515edd6f1593aa821b
karthikeyanff1239-tech <karthikeyanff1239@gmail.com>
2026-03-04 11:53:26 +0530

    Add files via upload
    Initial upload of React + Vite project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { useCart } from "../Context/CartContext";
+
+export default function Navbar() {
+  const { cartItems } = useCart();
+
+  return (
+    <nav>
+      <div className="logo">Shop</div>
+
+      <div className="demo">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/cart">
+          Cart ({cartItems.length})
+        </Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+}
